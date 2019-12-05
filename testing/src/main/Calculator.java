605e588af6f10e5facd448baa2b86b975b80dbfb
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-12-05 10:11:42 +0700

    02 Demonstrating TDD through walkthroughs and exercises: 007 TDD walkthrough part 2

--- testing/src/main/Calculator.java
+++ testing/src/main/Calculator.java
@@ -1,30 +1,19 @@
 package main;
 
 import java.math.BigDecimal;
-import java.util.Stack;
 
 public class Calculator {
 
-    private Stack<BigDecimal> values = new Stack<>();
+    private OperandStack values = new OperandStack();
 
 
     public BigDecimal getAccumulator() {
-
-        if (values.size() == 0)
-            return BigDecimal.ZERO;
-
         return values.peek();
     }
 
-
     public void setAccumulator(BigDecimal value) {
-
-        if (values.size() > 0)
-            values.pop();
-
-        values.push(value);
+        values.replaceTop(value);
     }
-
 
     public void enter() {
         values.push(getAccumulator());
